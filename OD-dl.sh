95a0aabfca076a1c472d5d651497569e749f7179
omg <omg@orionsbelt.space>
2018-05-02 00:06:35 -0800

    script completion
    debugged
    formatted
    error messages for which function fails

--- OD-dl.sh
+++ OD-dl.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 
 ################################################################################
 ####     Will quickly generate a urls.txt of a http open directory          ####
@@ -34,40 +35,55 @@
 ################################################################################
 
 # name of the folder that all output files will go.
-PROJECTFOLDER="$2"
+projectPATH=("${HOME}"/files/"$2")
 
 # Path to the urls.txt file. Is used as input for downloading with axel
-URLS="$HOME/files/$PROJECTFOLDER/urls.txt"
+URLS=("${HOME}"/files/"$2"/urls.txt)
 
-# Allows use of rclone with a http remote without a Config file.
-export RCLONE_CONFIG_ZZ_TYPE=http
-# Tells rclone what website its working with.
-export RCLONE_CONFIG_ZZ_URL=$1
+function jim() {
+  "$@" || { echo "he's dead jim: $@" && exit 1;}
+}
 
-# arguments for the rclone package.
-#  "--fast-list"  : "Use recursive list if available. Uses more memory but fewer transactions."
-# --all             All files are listed (list . files too).
-# --full-path       Print the full path prefix for each file.
-# --noindent        Don't print indentation lines.
-# --noreport        Turn off file/directory count at end of tree listing.
-# --checkers int                        Number of checkers to run in parallel. (default 8)
-# --output string   Output to file instead of stdout.
-rloneARGS="--all --fast-list --full-path --noindent --noreport --checkers=4 --output="$HOME/files/$PROJECTFOLDER""
+function project_folder() {
+  mkdir -p "${projectPATH[*]}"
+}
 
-# Arguments for the GNU parallel package.
-# "--link"        : "If multiple input sources are given, one argument will be read from each of the input sources."
-# "--jobs=n"      : "Run n jobs in parallel."
-# "--delay=n"     : "Delay the start of new jobs n amount of seconds."
-# "--joblog="     : "Path to logfile of the jobs completed so far."
-# "--shuf"        : "Shuffle order that jobs are ran."
-# " :::: "        : "argfiles. Unlike other options for GNU parallel :::: is placed after the command and before the arguments."
-parallelARGS="--link --jobs=4 --delay=3 --joblog=alexandria-parallel.log --shuf"
+function rclone_tree() {
+  # Arguments for the rclone package.
+  # "RCLONE_CONFIG_ZZ_TYPE="  : Allows use of rclone with a http remote without a Config file.
+  # "RCLONE_CONFIG_ZZ_URL="   : Tells rclone what website its working with.
+  # "--fast-list": "Use recursive list if available. Uses more memory but fewer transactions."
+  # "--all"      : "All files are listed (list . files too)."
+  # "--full-path": "Print the full path prefix for each file."
+  # "--noindent" : "Don't print indentation lines."
+  # "--noreport" : "Turn off file/directory count at end of tree listing."
+  # "--checkers=": "Number of checkers to run in parallel. (default 8)"
+  # "--output="  : "Output to file instead of stdout."
+  local rloneARGS=("--all" "--fast-list" "--noindent" "--full-path" "--noreport" "--checkers=4" "--output=${URLS[@]}")
+  RCLONE_CONFIG_ZZ_TYPE=http RCLONE_CONFIG_ZZ_URL=$1 rclone tree zz: "${rloneARGS[@]}"
+}
 
-# Arguments for the axel package.
-# "--output=x"    : "Data will be downloaded to this file or directory."
-# "--no-clobber"    : "Skip download if a file with the same name already exists in the current folder and no state file is found."
-axelARGS="--output="$HOME/files/$PROJECTFOLDER" --no-clobber"
+function OD_structure() {
+  local parallelARGS=("--jobs=4" "--joblog=${projectPATH[*]}/OD-dl.log")
+  parallel "${parallelARGS[@]}" mkdir -p "${projectPATH[*]}"{.} :::: "${URLS[@]}"
+}
 
-mkdir -P "$HOME"/files/"$PROJECTFOLDER"
-rclone tree zz: "$rloneARGS"
-parallel "$parallelARGS" axel "$axelARGS" "$1"{} :::: "$URLS"
+function parallelize() {
+  # Arguments for the GNU parallel package.
+  # "--link"        : "If multiple input sources are given, one argument will be read from each of the input sources."
+  # "--jobs=n"      : "Run n jobs in parallel."
+  # "--delay=n"     : "Delay the start of new jobs n amount of seconds."
+  # "--joblog="     : "Path to logfile of the jobs completed so far."
+  # "--shuf"        : "Shuffle order that jobs are ran."
+  # " :::: "        : "argfiles. Unlike other options for GNU parallel :::: is placed after the command and before the arguments."
+  local parallelARGS=("--jobs=4" "--delay=3" "--joblog=${projectPATH[*]}/OD-dl.log" "--shuf" "--link")
+  # Arguments for the axel package.
+  # "--output=x"  : "Data will be downloaded to this file or directory."
+  # "--no-clobber": "Skip download if a file with the same name already exists in the current folder and no state file is found."
+  local axelARGS=("--output=${projectPATH[*]}{2}" "--no-clobber")
+  parallel "${parallelARGS[@]}" axel ${axelARGS[@]} "${1}"{1} :::: "${URLS[@]}" "${URLS[@]}"
+}
+
+jim project_folder && jim rclone_tree "$@" &&  OD_structure "$@" && jim parallelize "$@"
+echo ""
+echo "Was that quick or what?"
